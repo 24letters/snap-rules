561f8dc819f998466cacc4e1337f6ddb5615b4e6
Anton Timmermans <email@atimmer.com>
2024-08-31 20:08:38 +0200

    Add more styling

--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -3,5 +3,13 @@ import { getImage } from "~/server/queries";
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
 
-  return <img src={image.url} className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0 bg-green-500">
+      <img src={image.url} className="flex-shrink object-contain" />
+
+      <div className="flex w-48 flex-col">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
+  );
 }

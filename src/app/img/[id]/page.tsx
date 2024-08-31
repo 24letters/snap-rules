8a89f5405709abbbc09626919d5ba8c7b0765da9
Anton Timmermans <email@atimmer.com>
2024-08-31 20:03:33 +0200

    Open images in modal

--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,7 +1,12 @@
+import FullPageImageView from "~/components/full-image-page";
+
 export default function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  return <div>{photoId}</div>;
+  const idAsNumber = parseInt(photoId, 10);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+  return <FullPageImageView id={idAsNumber} />;
 }

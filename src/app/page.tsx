dc740936459f1386d969b502c86830edd5ddacaf
Anton Timmermans <email@atimmer.com>
2024-08-31 17:24:38 +0200

    Require login for viewing gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,34 @@
 import Image from "next/image";
 import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => {
+        return (
+          <div key={image.id} className="flex w-48 flex-col">
+            <Image width={300} height={200} key={image.url} src={image.url} />
+            <div>{image.name}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {images.map((image) => {
-          return (
-            <div key={image.id} className="flex w-48 flex-col">
-              <Image width={300} height={200} key={image.url} src={image.url} />
-              <div>{image.name}</div>
-            </div>
-          );
-        })}
-      </div>
+      <SignedOut>Please sign in to view the gallery</SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
